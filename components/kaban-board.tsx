dbccbabcddc4c35ce55e57ea6c6a956832c68953
saikishan8055 <saikishan.sai01@gmail.com>
2026-02-11 23:22:09 +0530

    DashBoard

--- components/kaban-board.tsx
+++ components/kaban-board.tsx
@@ -1,13 +1,48 @@
 "use client"
 
 import { Board } from "@/lib/models/models.types"
+import { Award, Calendar, CheckCircle2, Mic, XCircle } from "lucide-react";
+import React from "react";
 
 interface KanBoardProps {
     board :Board ,
     userId: string,
 }
 
+const COLUMN_CONFIG :Array<{color : string; icon:React.ReactNode}>=[
+    {
+color:'bg-cyan-500',
+icon :<Calendar className="h-4 w-14"/>
+    },
+    {
+            color:'bg-purple-500',
+            icon:<CheckCircle2 className="h-4 w-4"/>
+     },
+     {
+        color:'bg-green-400',
+        icon:<Mic className="h-4 w-4"/>
+     },
+     {
+        color:'bg-yellow-500',
+        icon:<Award className="h-4 w-4"/>
+     },
+     {
+        color:'bg-red-500',
+        icon:<XCircle className="h-5 w-5"/>
+     }
+
+]
+
+
+
 export function  KanbanBoard({board,userId}:KanBoardProps){
-    return <></>
+    
+    return <>
+    <div>
+        <div>
+
+        </div>
+    </div>
+    </>
 
 }
